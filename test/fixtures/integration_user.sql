227ffa0c2e355400cbefe7d7a730439d9486bff0
sartim <sarrtim@gmail.com>
2026-09-02 18:26:25 +0300

    test: generate fixture password with postgres

--- test/fixtures/integration_user.sql
+++ test/fixtures/integration_user.sql
@@ -1,8 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 INSERT INTO public.users
     (first_name, last_name, email, password, is_deleted)
 VALUES
     ('Integration', 'Fixture', 'integration@example.test',
-     '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', FALSE)
+     crypt('integration-password', gen_salt('bf', 10)), FALSE)
 ON CONFLICT (email) DO UPDATE
 SET password = EXCLUDED.password,
     is_deleted = FALSE;
